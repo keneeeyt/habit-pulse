724bccf8c96de01fd92f93f0907aba295b5d8a72
Kenneth Louie Ermila Cervantes <111757975+keneeeyt@users.noreply.github.com>
2024-08-11 23:04:24 +0800

    F

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import HeroSection from "./_components/HeroSection";
 export default function Home() {
   return (
     <>
-      {" "}
+      {" "} 
       <Navbar />
       <HeroSection />
     </>
