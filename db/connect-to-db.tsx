4e64a5f6b9ae767eef92dce1c2078cf055382c2d
Kenneth Louie Ermila Cervantes <111757975+keneeeyt@users.noreply.github.com>
2024-08-11 22:02:46 +0800

    update connect to database

--- db/connect-to-db.tsx
+++ db/connect-to-db.tsx
@@ -1,13 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 async function connectToDb() {
   const mongodbUri = process.env.MONGODB_URI || "";
+
+  if (!mongodbUri) {
+    throw new Error("MONGODB_URI is not defined in environment variables.");
+  }
+
+  if (mongoose.connection.readyState >= 1) {
+    // If the connection is already established, return the existing connection
+    console.log("Using existing MongoDB connection");
+    return;
+  }
+
   try {
     await mongoose.connect(mongodbUri);
-
     console.log("Connected to MongoDB");
   } catch (err) {
-    console.log(err);
+    console.error("Error connecting to MongoDB:", err);
   }
 }
 
